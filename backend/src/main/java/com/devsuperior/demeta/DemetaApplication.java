105965a307636ba13fc6b6acd9fae955adf36455
Luis Ferrari <luisiferrari@gmail.com>
2022-09-08 22:59:22 -0400

    project created

---
+++ backend/src/main/java/com/devsuperior/demeta/DemetaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.demeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemetaApplication.class, args);
+	}
+
+}
