7cd46a1cfcdee0ede512af0b2f0ac15d94b8990a
Luis Ferrari <luisiferrari@gmail.com>
2022-09-09 19:17:05 -0400

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import Header from "./componentes/Header/Index"
 import SalesCard from "./componentes/SalesCard/Index"
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
     <>
+      <ToastContainer/>
       <Header />
       <main>
         <section id="sales">
