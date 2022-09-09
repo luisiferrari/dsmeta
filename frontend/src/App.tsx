9dc2614c36463ea0600e7b520cc572ef25353112
Luis Ferrari <luisiferrari@gmail.com>
2022-09-08 23:38:24 -0400

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+import NottificationButton from "./componentes/NotificationButton"
+
 function App() {
-  return(
-    <h1>Olá mundo</h1>
+  return (
+    <>
+      <h1>Olá mundo</h1>
+      <NottificationButton />
+      <NottificationButton />
+      <NottificationButton />
+      <NottificationButton />
+      <NottificationButton />
+    </>
   )
 }
 
